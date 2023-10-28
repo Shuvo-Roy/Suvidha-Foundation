5facd5f73f399d51df0c9823125793395488d04b
Shuvo Roy <shuvoroysp@gmail.com>
2023-10-28 15:38:02 +0600

    testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination";
 import "./swiper.css";
-import { Pagination } from "swiper/modules";
+import "swiper/css/pagination";
+//import 'swiper/css/effect-coverflow';
+
+import { Pagination,EffectCoverflow } from "swiper/modules";
 
 
 import testimonial from "../assets/testimonial/testimonial";
@@ -11,26 +13,31 @@ import testimonial from "../assets/testimonial/testimonial";
 export default function Testimonial() {
 
   return (
-    <div className="w-5/6 m-auto mb-8">
+    <div className="w-5/6 m-auto mb-12">
       <div className="flex flex-col items-center justify-center">
         <h1  className="text-yellow text-4xl">Testimonial</h1>
         <p  className="text-yellow text-5xl">What people are talking about our charity activities</p>
       </div>
       <div>
-        <Swiper
-          slidesPerView={4}
-          centeredSlides={true}
-          spaceBetween={30}
-          grabCursor={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className="mySwiper w-full h-full"
-        >
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        loop={true}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
         {testimonial.map((testItem,index)=>(
-            <SwiperSlide className="flex items-center justify-center rounded-xl overflow-hidden" key={index}>
-            <img src={testItem.img} alt="" className=" object-fit" />
+            <SwiperSlide key={index}>
+            <img src={testItem.img} alt="#"/>
           </SwiperSlide>
         ))}
           

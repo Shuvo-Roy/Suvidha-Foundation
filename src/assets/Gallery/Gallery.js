145fa5779cc7c74da7820d59ee25b7e70520c024
Shuvo Roy <shuvoroysp@gmail.com>
2023-10-29 15:04:22 +0600

    gallery

---
+++ src/assets/Gallery/Gallery.js
@@ -0,0 +1,59 @@
+import workShop from "./Workshops.jpg";
+import Book from "./BooksDistribution.jpg";
+import online from "./OnlineEvents.jpg";
+import cloths from "./ClothsDistribution.jpg";
+import tree from "./TreePlantation.jpg";
+import edu from "./FreeEducation.jpg";
+import womensDay from "./WomensDay.jpg";
+import food from "./FoodDistribution.jpg";
+import events from "./SuvidhaEvents.jpg";
+import empowering from "./SanitaryPad.jpg";
+import animal from "./AnimalFeeding.jpg";
+
+const gallery = [
+  {
+    name: "Free Workshop",
+    img: workShop,
+  },
+  {
+    name: "Books Distribution",
+    img: Book,
+  },
+  {
+    name: "Online Events",
+    img: online,
+  },
+  {
+    name: "Cloths Distribution",
+    img: cloths,
+  },
+  {
+    name: "Tree Plantation",
+    img: tree,
+  },
+  {
+    name: "Free Education",
+    img: edu,
+  },
+  {
+    name: "Women's Day",
+    img: womensDay,
+  },
+  {
+    name: "Food Distribution",
+    img: food,
+  },
+  {
+    name: "Suvidha Events",
+    img: events,
+  },
+  {
+    name: "Empowering Women",
+    img: empowering,
+  },
+  {
+    name: "Animal Feeding",
+    img: animal,
+  },
+];
+ export default gallery
--- src/components/Pages/Gallery.jsx
+++ src/components/Pages/Gallery.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import gallery from '../../assets/Gallery/Gallery.js'
 export default function Gallery() {
   return (
-    <div>Gallery</div>
-  )
+    <div className="mt-8 mb-8">
+      <div className="w-5/6 m-auto">
+        <div className="flex flex-col items-center justify-center m-4 p-4">
+          <h1 className="text-yellow text-4xl p-4">Our Gallery</h1>
+          <p className="text-slate text-5xl">
+            Moments of Impact: Capturing the Journey of Our NGO
+          </p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 justify-around">
+        {gallery.map((gallery,item)=>(
+          <div className="flex flex-col items-center justify-center rounded-md overflow-hidden bg-sky bg-opacity-10" key={item}>
+          <div className="">
+            <img src={gallery.img} className="rounded-md p-1"/>
+          </div>
+          <div>
+            <p className="text-3xl m-2 p-2">{gallery.name}</p>
+          </div>
+          </div>
+        ))}
+          
+        </div>
+      </div>
+    </div>
+  );
 }
